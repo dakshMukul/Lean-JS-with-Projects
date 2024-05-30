22bc8ab1c68f773884c4cb736034ec80e575fbf4
Mukul Singh <dakshmukul15@gmail.com>
2024-05-30 11:46:08 +0530

    added countdown timer logic

--- Countdown Timer/index.js
+++ Countdown Timer/index.js
@@ -1,27 +1,84 @@
 (function () {
-  const hour = document.querySelector(".hour");
-  const mins = document.querySelector(".minute");
-  const sec = document.querySelector(".sec");
-  const resetBtn = document.querySelector(".reset");
-  const startBtn = document.querySelector(".start");
+  var hour = document.querySelector(".hour");
+  var min = document.querySelector(".minute");
+  var sec = document.querySelector(".sec");
+  var startBtn = document.querySelector(".start");
+  var stopBtn = document.querySelector(".stop");
+  var resetBtn = document.querySelector(".reset");
 
-  resetBtn.addEventListener("click", () => {
-    sec.value = "";
-    hour.value = "";
-    mins.value = "";
-  });
+  var countdownTimer = null;
 
-  startBtn.addEventListener("click", startTimer);
+  // Start Timer Button - START
+  startBtn.addEventListener("click", function () {
+    if (hour.value == 0 && min.value == 0 && sec.value == 0) return;
 
-  function startTimer() {
-    console.log("start btn clicked");
-    const time = sec.value;
-    let secCounter = 0;
-    for (let i = time; i >= 0; i--) {
-      setTimeout(() => {
-        sec.value = i;
-      }, 1000 * secCounter);
-      secCounter++;
+    function startInterval() {
+      startBtn.style.display = "none";
+      stopBtn.style.display = "initial";
+
+      countdownTimer = setInterval(function () {
+        timer();
+      }, 1000);
     }
+    startInterval();
+  });
+  // Start Timer Button - END
+
+  function timer() {
+    // Formatting the time - START
+    if (sec.value > 60) {
+      min.value++;
+      sec.value = parseInt(sec.value) - 59;
+    }
+    if (min.value > 60) {
+      hour.value++;
+      min.value = parseInt(min.value) - 60;
+    }
+    min.value = min.value > 60 ? 60 : min.value;
+    // Formatting the time - END
+
+    // Updating the Time - START
+    if (hour.value == 0 && min.value == 0 && sec.value == 0) {
+      hour.value = "";
+      min.value = "";
+      sec.value = "";
+      stopInterval();
+    } else if (sec.value != 0) {
+      sec.value = `${sec.value <= 10 ? "0" : ""}${sec.value - 1}`;
+    } else if (min.value != 0 && sec.value == 0) {
+      sec.value = 59;
+      min.value = `${min.value <= 10 ? "0" : ""}${min.value - 1}`;
+    } else if (hour.value != 0 && min.value == 0) {
+      min.value = 60;
+      hour.value = `${hour.value <= 10 ? "0" : ""}${hour.value - 1}`;
+    }
+    return;
+    // Updating the Time - END
   }
+
+  // Stop Interval Logic - START
+  function stopInterval(state) {
+    startBtn.innerHTML = state === "pause" ? "Continue" : "Start";
+
+    stopBtn.style.display = "none";
+    startBtn.style.display = "initial";
+    clearInterval(countdownTimer);
+  }
+  // Stop Interval Logic - END
+
+  // Stop Timer Button - START
+  stopBtn.addEventListener("click", function () {
+    stopInterval("pause");
+  });
+  // Start Timer Button - END
+
+  // Reset Timer Button - START
+  resetBtn.addEventListener("click", function () {
+    hour.value = "";
+    min.value = "";
+    sec.value = "";
+
+    stopInterval();
+  });
+  // Reset Timer Button - END
 })();
